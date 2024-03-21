c80d46ffe9c97a2a000b7817ac46c15732f563a4
dindasudita <dindasudita2016@gmail.com>
2024-03-21 15:04:47 +0700

    first commit

---
+++ dinda-project/src/components/Buttonc.vue
@@ -0,0 +1,16 @@
+<!-- adalah sintaks dasar dari vue, harus ada -->
+<template> 
+    <button class="btn">
+        submit
+    </button>
+</template>
+
+<!-- css vue 3 -->
+<!-- scoped berarti style ini hanya berlaku di bagian ini -->
+<style scoped> 
+.btn {
+    background-color: red;
+    color: white;
+    font-size: 28px;
+}
+</style>
